0f2e8065b46e8c7313772cf6486879579ae751a2
afcbrandon <128235286+afcbrandon@users.noreply.github.com>
2026-08-13 16:10:10 -0500

    Implemented the code for relational data ingestion and merging of the datasets by zip code

--- src/ingestion.py
+++ src/ingestion.py
@@ -1,1 +1,15 @@
+import pandas as pd
 
+
+def LoadMergeData(churn_path, pop_path):
+    df_churn = pd.read_csv(churn_path)
+    df_pop = pd.read_csv(pop_path)
+    merged_df = pd.merge(df_churn, df_pop, on="Zip Code", how="left")
+    return merged_df
+
+
+if __name__ == "__main__":
+    df = LoadMergeData(
+        "../telecom_customer_churn.csv", "../telecom_zipcode_population.csv"
+    )
+    print(f"Ingested & merged dataset: {df.shape[0]} rows, {df.shape[1]} columns")
